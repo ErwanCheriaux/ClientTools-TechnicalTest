d4fe4d805c3ec0d98a3d6126d39ff41015c4bb11
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2021-05-22 14:23:12 +1200

    added ValidPartNumber private method to PartCatalogue

--- src/PartsTrader.ClientTools/PartCatalogue.cs
+++ src/PartsTrader.ClientTools/PartCatalogue.cs
@@ -9,9 +9,34 @@ namespace PartsTrader.ClientTools
     {
         public IEnumerable<PartSummary> GetCompatibleParts(string partNumber)
         {
-            //TODO: implement
+            ValidPartNumber(partNumber);
 
             throw new NotImplementedException();
+        }
+
+        private void ValidPartNumber(string value)
+        {
+            string[] partNumber = value.Split('-');
+
+            // check if there is only one dash between partId and partCode
+            if (partNumber.Length != 2) throw new InvalidPartException($"The specified value hasn't one dash only: '{partNumber}'.");
+
+            string partId = partNumber[0];
+            string partCode = partNumber[1];
+
+            //partId check
+            if (partId.Length != 4) throw new InvalidPartException($"The specified value doesn't contain 4 digits before the dash: '{partNumber}'.");
+            foreach (char c in partId)
+            {
+                if (!char.IsDigit(c)) throw new InvalidPartException($"The specified value doesn't contains digits only before the dash: '{partNumber}'.");
+            }
+
+            //partCode check
+            if (partCode.Length < 4) throw new InvalidPartException($"The specified value contains less than 4 alphanumeric char after the dash: '{partNumber}'.");
+            foreach (char c in partCode)
+            {
+                if (!char.IsLetterOrDigit(c)) throw new InvalidPartException($"The specified value doesn't contains alphanumeric char only after the dash: '{partNumber}'.");
+            }
         }
     }
 }
