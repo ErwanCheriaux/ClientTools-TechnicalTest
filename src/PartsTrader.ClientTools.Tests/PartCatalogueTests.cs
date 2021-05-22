1a4017ad8a06e5a5f92b8ce6e632cf23b80e2751
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2021-05-22 13:07:45 +1200

    added content of PartsTrader.ClientTools_2020.zip

---
+++ src/PartsTrader.ClientTools.Tests/PartCatalogueTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace PartsTrader.ClientTools.Tests
+{
+    /// <summary>
+    /// Tests for <see cref="PartCatalogue" />.
+    /// </summary>
+    [TestFixture]
+    public class PartCatalogueTests
+    {
+        //TODO: include your unit tests here
+    }
+}
---
+++ src/PartsTrader.ClientTools/PartCatalogue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using PartsTrader.ClientTools.Api;
+using PartsTrader.ClientTools.Api.Data;
+
+namespace PartsTrader.ClientTools
+{
+    public class PartCatalogue : IPartCatalogue
+    {
+        public IEnumerable<PartSummary> GetCompatibleParts(string partNumber)
+        {
+            //TODO: implement
+
+            throw new NotImplementedException();
+        }
+    }
+}
